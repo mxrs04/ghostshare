b2dffe4674cb52d6d0700aeb364bbe89f83ed726
mxrs04 <st193511@stud.uni-stuttgart.de>
2026-01-05 12:20:07 +0100

    Fix: Add base path for GitHub Pages and update workflow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ghostshare/', // <--- DAS MUSS DRIN SEIN
+  base: '/ghostshare/', // <--- WICHTIG: Das hier muss rein! (Name deines Repos)
   plugins: [
     vue(),
+    vueJsx(),
     vueDevTools(),
   ],
   resolve: {
